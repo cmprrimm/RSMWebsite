8c37fad2e397d864ad1666357556ce205ac0e51d
cmprrimm <r.m.rimmer@2017.ljmu.ac.uk>
2020-11-23 15:15:59 +0000

    structuring the login page

--- RSMWeb/login.php
+++ RSMWeb/login.php
@@ -5,14 +5,34 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
-<link rel="stylesheet" href="bootstrap/css/slideshow.css">
 <script src="bootstrap/js/bootstrap.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
- <?php include "Includes/navBar.php.php" ?>
 </head>
 <body>
-<div id=ourServicesJumbo class="jumbotron">
-  <h1 class="display-4">RSM GP Access</h1>
-</div>
+	<div id=ourServicesJumbo class="jumbotron-fluid">
+		<h1 class="display-4">RSM GP Access</h1>
+	</div>
+
+	<div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-6">
+                    <h3>Login</h3>
+                    <form>
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Your Email *" value="" />
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" placeholder="Your Password *" value="" />
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" class="btnSubmit" value="Login" />
+                        </div>
+                        <div class="form-group">
+                            <a href="#" class="ForgetPwd">Forget Password?</a>
+                        </div>
+                    </form>
+                </div>
+                </div>
+                </div>
 </body>
 </html>
