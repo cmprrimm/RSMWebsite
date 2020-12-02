b7819ed94510b1d0b35c676f9164e1067e4c7666
cmprrimm <r.m.rimmer@2017.ljmu.ac.uk>
2020-12-02 17:57:20 +0000

    Completing the website ready for PHP

--- RSMWeb/Includes/navBar.php
+++ RSMWeb/Includes/navBar.php
@@ -1,10 +1,10 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="login.php">LOGO</a>
+  <a class="navbar-brand" href="login.php">RSM</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-    <form class="form-inline my-2 my-lg-0 ">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
+    <a class="btn btn-primary" href="newPatient.php" role="button">Find Patient</a>
+    <a class="btn btn-primary" href="account.php" role="button">My Account</a>
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">logout</button>
+
 </nav>
